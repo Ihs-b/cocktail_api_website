3db269ea0ea5ad8997ff5e4130fa9643cc15198b
Ihssane <tefl.ben10@gmail.com>
2023-02-15 13:51:04 +0100

    hiding secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,7 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3jn18=c9yf)6$ddb2djt7co@huj2hmie6v5ndtc78yvo=5orv%'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
